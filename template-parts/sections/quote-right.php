4476989b24a66901e7b3aed04b8a783922def3c8
Ilya8741 <ilyha874@mail.ru>
2025-09-25 14:24:55 +0300

    aos fix

--- template-parts/sections/quote-right.php
+++ template-parts/sections/quote-right.php
@@ -1,5 +1,5 @@
-<div class="quote-right" data-aos="fade-left">
-    <div class="quote-right-wrapper">
+<div class="quote-right">
+    <div class="quote-right-wrapper" data-aos="fade-left">
         <div class="quote-right-content">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="27" viewBox="0 0 40 27" fill="none">
                 <path d="M21.2174 27L22.8406 16.0189L35.0145 0H40L31.5362 14.2075L36.4058 14.4906L34.5507 27H21.2174ZM0 27L1.68116 16.0189L13.7971 0H18.7826L10.3188 14.2075L15.2464 14.4906L13.3333 27H0Z" fill="#717171" />
