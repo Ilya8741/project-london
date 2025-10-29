cbe8f8eabe9b40fce0ef378b42e9f9de13584421
Ilya8741 <ilyha874@mail.ru>
2025-10-29 14:30:12 +0300

    careers hero

---
+++ template-parts/sections/careers-hero.php
@@ -0,0 +1,21 @@
+<div class="careers-hero-sec">
+  <div class=" careers-hero-sec-wrapper">
+    <div class="main-container">
+      <h1 class="careers-hero-sec-title">
+        <?php the_sub_field('hero_title'); ?>
+      </h1>
+    </div>
+
+    <div class="careers-hero-sec-image-wrapper">
+      <?php
+      $hero_video = get_sub_field('video'); 
+      ?>
+
+      <?php if ($hero_video): ?>
+        <video autoplay muted loop playsinline>
+          <source src="<?php echo esc_url($hero_video['url']); ?>" type="<?php echo esc_attr($hero_video['mime_type']); ?>">
+        </video>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
